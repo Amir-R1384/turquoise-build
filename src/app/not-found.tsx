ad34abdd58c43b129676e75da0e5511543dd618b
Amir Ranjbar <ranjbaramirreza5786@yahoo.com>
2024-01-28 21:22:49 -0500

    Added french support for not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,34 @@
+'use client'
+
 import { Source_Serif_4 } from 'next/font/google'
 import Link from 'next/link'
 import './[lang]/globals.css'
 
 const source_serif_4 = Source_Serif_4({ subsets: ['latin'], weight: ['200', '300', '400', '500'] })
 
+const translations = {
+	noPage: {
+		en: "The page you're looking for doesn't exist or it has been removed",
+		fr: "La page que vous cherchez n'existe pas ou elle a été enlevée"
+	},
+	return: {
+		en: 'Return to Home Screen',
+		fr: 'Retourner à la maison'
+	}
+}
+
 export default function notFound() {
+	const lang = navigator.language.toLowerCase().includes('fr') ? 'fr' : 'en'
+
 	return (
 		<div
 			className={
-				source_serif_4.className + ' flex text-center flex-col items-center gap-y-10 pt-20'
+				source_serif_4.className +
+				' px-main flex text-center flex-col items-center gap-y-10 pt-20'
 			}>
-			<h1 className="text-4xl ">
-				The page you&apos;re looking for doesn&apos;t exist or it has been removed
-			</h1>
-			<Link href="/" className="button">
-				Return to Home Screen
+			<h1 className="text-4xl ">{translations.noPage[lang]}</h1>
+			<Link href={lang === 'en' ? '/' : '/fr'} className="button">
+				{translations.return[lang]}
 			</Link>
 		</div>
 	)
